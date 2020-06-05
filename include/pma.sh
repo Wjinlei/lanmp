df0e8c8c586a2b9590a7d4a8f5670ac7e63a1015
Wjinlei <1976883731@qq.com>
2020-06-05 14:13:54 +0800

    fix bug pma

--- include/pma.sh
+++ include/pma.sh
@@ -6,23 +6,22 @@ install_pma(){
             echo "Create directory failed: ${1}" && exit 1
         fi
         chown -R www:www ${1}
-    else
-        rm -fr ${1}/pma
-        cd /tmp
-        _info "${phpmyadmin_filename} install start..."
-        DownloadFile "${phpmyadmin_filename}.tar.gz" "${phpmyadmin_download_url}"
-        rm -fr ${phpmyadmin_filename}
-        tar zxf ${phpmyadmin_filename}.tar.gz
-        mv ${phpmyadmin_filename} ${1}/pma
-        wget --no-check-certificate -cv -t3 -T60 "https://d.hws.com/free/hwslinuxmaster/conf/phpmyadmin-conf.tar.gz"
-        rm -f /tmp/config.inc.php
-        tar zxf phpmyadmin-conf.tar.gz
-        cp -f config.inc.php ${1}/pma/config.inc.php
-        mkdir -p ${1}/pma/{upload,save}
-        local tmpUser=$(ls -ld ${1}|awk '{print $3}')
-        local tmpGroup=$(ls -ld ${1}|awk '{print $4}')
-        chown -R ${tmpUser}:${tmpGroup} ${1}/pma
-        _success "${phpmyadmin_filename} install completed..."
-        rm -f /tmp/config.inc.php
     fi
+    rm -fr ${1}/pma
+    cd /tmp
+    _info "${phpmyadmin_filename} install start..."
+    DownloadFile "${phpmyadmin_filename}.tar.gz" "${phpmyadmin_download_url}"
+    rm -fr ${phpmyadmin_filename}
+    tar zxf ${phpmyadmin_filename}.tar.gz
+    mv ${phpmyadmin_filename} ${1}/pma
+    wget --no-check-certificate -cv -t3 -T60 "https://d.hws.com/free/hwslinuxmaster/conf/phpmyadmin-conf.tar.gz"
+    rm -f /tmp/config.inc.php
+    tar zxf phpmyadmin-conf.tar.gz
+    cp -f config.inc.php ${1}/pma/config.inc.php
+    mkdir -p ${1}/pma/{upload,save}
+    local tmpUser=$(ls -ld ${1}|awk '{print $3}')
+    local tmpGroup=$(ls -ld ${1}|awk '{print $4}')
+    chown -R ${tmpUser}:${tmpGroup} ${1}/pma
+    _success "${phpmyadmin_filename} install completed..."
+    rm -f /tmp/config.inc.php
 }
