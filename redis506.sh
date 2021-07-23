9322b37af29bf957695d81929db78454865a8575
Wjinlei <1976883731@qq.com>
2021-07-23 09:25:50 +0800

    Optimized

--- redis506.sh
+++ redis506.sh
@@ -23,7 +23,7 @@ _create_sysv_script(){
 # description: redis service script
 
 ### BEGIN INIT INFO
-# Provides:          redis506
+# Provides:          redis
 # Required-Start:    $all
 # Required-Stop:     $all
 # Default-Start:     2 3 4 5
